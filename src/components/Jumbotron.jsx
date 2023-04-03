4140949716ac5e43f024d5b7050f9cd3b6836ef9
Justen Anderson <justenanderson@gmail.com>
2023-04-03 11:08:31 -0600

    Add scroll to learn more section.

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,15 @@ import Iphone from '../assets/images/iphone-14.jpg'
 import HoldingIphone from '../assets/images/iphone-hand.png'
 
 const Jumbotron = () => {
+  const handleLearnMore = () => {
+    const element = document.querySelector('.sound-section')
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <div className="jumbotron-section wrapper">
       <h2 classHane='title'>New</h2>
@@ -14,7 +23,7 @@ const Jumbotron = () => {
           <button className='button'>Buy</button>
         </li>
         <li>
-          <a className='link'>Learn more</a>
+          <a className='link' onClick={ handleLearnMore }>Learn more</a>
         </li>
 
       </ul>
