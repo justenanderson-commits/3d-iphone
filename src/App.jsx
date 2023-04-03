28d045747153cc9e066b858f961c1610a0637aae
Justen Anderson <justenanderson@gmail.com>
2023-04-03 09:47:42 -0600

    Add jumbotron below nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Nav from './components/Nav'
+import Jumbotron from './components/Jumbotron';
 
 function App() {
 
   return (
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron />
     </div>
   );
 }
